c61b3a3f82baef85dac5f791eeb4e88f6b4ac14e
merecat25 <79212435+merecat25@users.noreply.github.com>
2025-07-10 14:54:50 -0400

    Update app.py with myanmar chart

--- python-scripts/app.py
+++ python-scripts/app.py
@@ -74,7 +74,7 @@ app.layout = html.Div(children=[
     # ---------- First Static Chart ----------
     html.Div([
         html.Img(
-            src='/assets/iran_ping2_rtt.png',
+            src='/assets/myanmar.png',
             style={
                 'width': '100%',
                 'maxWidth': '1000px',
